5c078c67d47913b628fcbb05450376360deae885
Wajahat Ullah <wajahatullah235@gmail.com>
2025-07-15 12:06:02 +0500

    Add new projects

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -3,7 +3,7 @@ export const projectsData = [
     {
         id: 1,
         name: 'DIPCar',
-        description: 'Developed a cost-effective autonomous driving prototype powered by Jetson Nano. Leveraged optimized deep learning models for environment perception and control to enable autonomous navigation on economical hardware.',
+        description: 'Developed a cost-effective autonomous driving prototype powered by Jetson Nano. Leveraged optimized deep learning models for environment perception and control to enable autonomous navigation on economical hardware',
         tools: ['Python', 'Jetson Nano', 'Deep Learning', 'OpenCV'],
         role: 'Computer Vision Developer',
         code: '',
@@ -13,7 +13,7 @@ export const projectsData = [
     {
         id: 2,
         name: 'Personal Protective Equipment Detection',
-        description: 'Built a real-time detection system for Personal Protective Equipment in industrial settings. Modified Ultralytics’ YOLOv8 with custom MobileNet and ResNet50 backbones to ensure worker safety through accurate PPE detection.',
+        description: 'Built a real-time detection system for Personal Protective Equipment in industrial settings. Modified Ultralytics’ YOLOv8 with custom MobileNet and ResNet50 backbones to ensure worker safety',
         tools: ['Python', 'YOLOv8', 'MobileNet', 'ResNet50', 'Ultralytics', 'OpenCV'],
         role: 'Computer Vision Developer',
         code: '',
@@ -23,7 +23,7 @@ export const projectsData = [
     {
         id: 3,
         name: 'Bone Crack Recognition',
-        description: 'Developed an X-ray classification system to assist radiologists in diagnosing bone fractures. Used transfer learning with ConvNeXt models to classify bone cracks from X-ray images, improving diagnostic accuracy.',
+        description: 'Developed an X-ray classification system to assist radiologists in diagnosing bone fractures. Used transfer learning with ConvNeXt models to classify bone cracks from X-ray images, improving diagnostic accuracy',
         tools: ['Python', 'ConvNeXt', 'Transfer Learning', 'PyTorch'],
         role: 'Machine Learning Developer',
         code: '',
