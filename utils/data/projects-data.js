1127a671c3d3a3ca3763b99d1c082850d40e8548
Wajahat Ullah <wajahatullah235@gmail.com>
2025-07-15 12:04:50 +0500

    Add new projects

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,48 +1,34 @@
-import ayla from '/public/image/ayla.jpg';
-import crefin from '/public/image/crefin.jpg';
-import travel from '/public/image/travel.jpg';
-import realEstate from '/public/image/real-estate.jpg';
 
 export const projectsData = [
     {
         id: 1,
-        name: 'AI Powered Financial App',
-        description: "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset. Voice messages are converted to text using AWS Transcribe. The app fetches data from Google Sheets and generates a PDF term sheet, sent via AWS SES.",
-        tools: ['Express', 'MongoDB', 'OpenAI API', 'AWS SES', 'AWS S3', 'Node Mailer', 'Joi', 'Puppeteer', 'EC2', 'PM2', 'Nginx'],
-        role: 'Backend Developer',
+        name: 'DIPCar',
+        description: 'Developed a cost-effective autonomous driving prototype powered by Jetson Nano. Leveraged optimized deep learning models for environment perception and control to enable autonomous navigation on economical hardware.',
+        tools: ['Python', 'Jetson Nano', 'Deep Learning', 'OpenCV'],
+        role: 'Computer Vision Developer',
         code: '',
         demo: '',
-        image: crefin,
+        image: '',
     },
     {
         id: 2,
-        name: 'Travel Agency App',
-        description: 'I have designed and developed a full-stack web app for 2Expedition, a travel agency in Armenia. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Typescript, MySQL, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.',
-        tools: ['NextJS', 'Tailwind CSS', "Google Maps", "NestJS", "TypeScript", "MySQL", "AWS S3", "Sun-Editor", "Gmail Passkey"],
-        role: 'Full Stack Developer',
+        name: 'Personal Protective Equipment Detection',
+        description: 'Built a real-time detection system for Personal Protective Equipment in industrial settings. Modified Ultralytics’ YOLOv8 with custom MobileNet and ResNet50 backbones to ensure worker safety through accurate PPE detection.',
+        tools: ['Python', 'YOLOv8', 'MobileNet', 'ResNet50', 'Ultralytics', 'OpenCV'],
+        role: 'Computer Vision Developer',
         code: '',
         demo: '',
-        image: travel,
+        image: '',
     },
     {
         id: 3,
-        name: 'AI Powered Real Estate',
-        description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
-        tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
-        code: '',
-        role: 'Full Stack Developer',
-        demo: '',
-        image: realEstate,
-    },
-    {
-        id: 4,
-        name: 'Newsroom Management',
-        description: "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
-        tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
+        name: 'Bone Crack Recognition',
+        description: 'Developed an X-ray classification system to assist radiologists in diagnosing bone fractures. Used transfer learning with ConvNeXt models to classify bone cracks from X-ray images, improving diagnostic accuracy.',
+        tools: ['Python', 'ConvNeXt', 'Transfer Learning', 'PyTorch'],
+        role: 'Machine Learning Developer',
         code: '',
         demo: '',
-        image: ayla,
-        role: 'Full Stack Developer',
+        image: '',
     }
 ];
 
